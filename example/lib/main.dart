c77b14a10798c0c04d65d2b9044fa43cb1a8039c
Suraj <surajj.verma7@gmail.com>
2022-07-14 14:31:23 +0530

    Fix Bugs

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -20,9 +20,9 @@ class _MyAppState extends State<MyApp> {
   Future<void> _oktaAuthenticationHandler() async {
     var config = OktaConfig(
       clientId: '0oa5r05zy5BV26wcQ5d7',
-      discoveryUri: ' https://dev-03370337-admin.okta.com:/oauth2/default',
+      discoveryUri: 'https://dev-03370337-admin.okta.com:/oauth2/default',
       redirectUri: 'com.okta.dev-03370337:/callback',
-      endSessionRedirectUri: ' https://dev-03370337-admin.okta.com:/',
+      endSessionRedirectUri: 'https://dev-03370337-admin.okta.com:/',
       scopes: ['openid', 'profile', 'email'],
     );
     var status = await _okta.createOIDCConfig(config);
